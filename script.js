4e3c92c8d21783d6917ddbf8d6da4d27a9c50099
Ibrahimzene <131875836+Ibrahimzene@users.noreply.github.com>
2023-10-18 14:11:02 +0200

    fetch

---
+++ script.js
@@ -0,0 +1,19 @@
+fetch("https://fakestoreapi.com/products")
+  .then((response) => {
+    return response.json();
+  })
+  .then((products) => {
+    document.getElementById("cards").innerHTML = `
+    ${products
+      .map((product) => {
+        return `
+    <div class="card">
+  <h2 class="title-card">${product.title}</h2>
+  <img src="${product.image}" alt="image" class="img-card"/>
+  <p>${product.description}</p>
+  <p class="category">${product.category}</p>
+  <p class="price">${product.price}</p>
+  </div>`;
+      })
+      .join("")}`;
+  });
